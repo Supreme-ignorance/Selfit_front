041bc366d833dcf07a781567bd525440662ce6f7
jay <anastia0609@gmail.com>
2022-11-20 19:45:57 +0900

    ?

--- src/components/Article/ArticleList.vue
+++ src/components/Article/ArticleList.vue
@@ -5,7 +5,7 @@
     </div>
     <v-divider></v-divider>
     <div class="mt-3 d-flex flex-row-reverse">
-      <v-btn color="blue lighten-4" elevation="2" class="mr-4" @click="login()">
+      <v-btn color="blue lighten-4" elevation="2" class="mr-4" @click="writeArticle()">
         글 쓰기
       </v-btn>
     </div>
