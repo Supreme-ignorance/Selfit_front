5c6451255eb52a569f2625045b4d5b05c781c6d8
jay <treeleaves22@naver.com>
2022-11-21 16:34:50 +0900

    banner

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,42 @@
 <template>
   <div>
-    <v-layout justify-center>
-      <h3 class="mx-auto my-12 align-center flex-column">셀핏에 오신 것을 환영합니다</h3>
-
-    </v-layout>
     <div>
-
+      <v-banner id="banner" class="pa-0" dark>
+      <v-carousel id="carousel" hide-delimiters>
+        <v-carousel-item
+          v-for="(item, i) in items"
+          :key="i"
+          :src="item.src">
+        </v-carousel-item>
+      </v-carousel>
+      </v-banner>
     </div>
-
-
-
-
-
-
-
+    
+    <!-- <div class="mx-auto my-12 align-center flex-column">
+      <h3>셀핏에 오신 것을 환영합니다</h3>
+    </div> -->
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      items: [
+      {
+        src: require("../assets/yoga_illustrate.png")
+      },
+      {
+        src: require("../assets/main1.jpg")
+      },
+      {
+        src: require("../assets/main2.jpg"),
+      },
+      {
+        src: require("../assets/yoga_park.jpg")
+      },
+      ]
+    }
+  }
+};
 </script>
