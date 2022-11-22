20d03a108206208d7d04166a524829021ab13074
Supreme-ignorance <rjsdlzoq@naver.com>
2022-11-22 12:17:14 +0900

    "개인 페이지 초안"

--- src/components/User/UserDetail.vue
+++ src/components/User/UserDetail.vue
@@ -1,8 +1,47 @@
 <template>
   <div>
-    <v-container>
-      <CalendarHeatmap :end-date="Date.now()" :values="getDaily"
-    /></v-container>
+    <v-card
+      class="mx-auto my-12 align-center flex-column"
+      :max-width="getContentWidth"
+      tile
+      outlined
+    >
+      <v-container>
+        <v-row dense>
+          <v-col cols="12">
+            <v-card color="#385F73" dark>
+              <v-card-title class="text-h5"> Unlimited music now </v-card-title>
+
+              <v-container>
+                <v-progress-linear
+                  color="light-blue"
+                  height="20"
+                  value="10"
+                  striped
+                ></v-progress-linear>
+              </v-container>
+            </v-card>
+          </v-col>
+        </v-row>
+
+        <v-row dense>
+          <v-col cols="12">
+            <v-card>
+              <v-card-title class="text-h5"> Unlimited music now </v-card-title>
+
+              <v-container>
+                <CalendarHeatmap :end-date="Date.now()" :values="getDaily"
+              /></v-container>
+
+              <v-card-subtitle
+                >Listen to your favorite artists and albums whenever and
+                wherever, online and offline.</v-card-subtitle
+              >
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
   </div>
 </template>
 
@@ -16,7 +55,7 @@ export default {
     CalendarHeatmap,
   },
   computed: {
-    ...mapGetters(["getDaily"]),
+    ...mapGetters(["getDaily", "getContentWidth"]),
   },
   created() {
     this.$store.dispatch("setDaily", "ssafy");
